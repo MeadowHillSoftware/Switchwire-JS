fa5a86de932a4678ed48a220e4e91a6f72961616
MeadowHillSoftware <meadowhillsoftware@aol.com>
2018-04-06 22:06:09 -0700

    multiple processes

--- switchwire.js
+++ switchwire.js
@@ -2,16 +2,35 @@
 
 var oSwitchwire = {};
 
+oSwitchwire.aHitList = ["geany", "clementine", "mousepad"];
+
 oSwitchwire.exec = require('child_process').exec;
 
-oSwitchwire.detectProcess = function(error, stdout, stderr) {
-    var sId = stdout
-    if (typeof sId === "string") {
-        console.log(sId);
-        oSwitchwire.exec("killall geany", oSwitchwire.callback);
+oSwitchwire.getProcessName = function(error, stdout, stderr) {
+    var sId = stdout;
+    if (sId !== "") {
+        var sCommand = "ps -fp " + sId;
+        oSwitchwire.exec(sCommand, oSwitchwire.killProcess);
     }
 };
 
-oSwitchwire.sCommand = "pidof geany";
+oSwitchwire.detectMultipleProcesses = function() {
+    var aHitList = oSwitchwire.aHitList;
+    for (var i = 0; i < aHitList.length; i++) {
+        var sProcess = aHitList[i];
+        var sCommand = "pidof " + sProcess;
+        oSwitchwire.exec(sCommand, oSwitchwire.getProcessName);
+    }
+};
 
-oSwitchwire.exec(oSwitchwire.sCommand, oSwitchwire.detectProcess);
+oSwitchwire.killProcess = function(error, stdout, stderr) {
+    var sOutput = stdout;
+    var aOutput = sOutput.split("/");
+    var iIndex = (aOutput.length - 1)
+    var sSubstring = aOutput[iIndex]
+    var sProcess = sSubstring.slice(0, -1);
+    var sCommand = "killall " + sProcess;
+    oSwitchwire.exec(sCommand);
+};
+
+oSwitchwire.detectMultipleProcesses();
